2912c4efb07a4f0aad14eba07be1dd34855115b3
Ian Schoenrock <ischoenrock@lumina247.net>
2019-12-13 11:18:04 -0500

    Initial Commit

---
+++ BottomDrawerPRactice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomDrawerPRactice
+//
+//  Created by ian schoenrock on 12/13/19.
+//  Copyright © 2019 ian schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
